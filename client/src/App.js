48ade4a8255f5f2967e806c8c3175e60449e3970
Zachary-Maddox <z19s96p@aol.com>
2022-08-22 19:24:34 -0500

    added a about me, Needs styling and images added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
-import logo from "./logo.svg";
+import AboutMe from "./AboutMe";
+import Navbar from "./Navbar";
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <Navbar/>
+        <AboutMe/>
+    </div>;
 }
 
 export default App;
