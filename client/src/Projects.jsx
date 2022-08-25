d891c451609841cb9ea59cbd92e45ae2bbca03f5
Zachary-Maddox <z19s96p@aol.com>
2022-08-24 21:16:27 -0500

    reworked issues

--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {SiHandlebarsdotjs,SiJavascript,SiMysql} from 'react-icons/si';
-import {IoLogoCss3 } from 'react-icons/io';
-import { FaHtml5,FaNodeJs } from 'react-icons/fa';
+import { SiHandlebarsdotjs, SiJavascript, SiMysql } from "react-icons/si";
+import { IoLogoCss3 } from "react-icons/io";
+import { FaHtml5, FaNodeJs } from "react-icons/fa";
 // bring in icon like you did on about me and make them a prop
 
 function Projects() {
@@ -16,51 +16,69 @@ function Projects() {
                     src="./images/game-Getter.png"
                 />
                 <div className="basis-1/2">
-                <h3 className=" text-end   m-4">Game Getter</h3>
-                <p className="indent-2 mb-4 mx-4">
-                    Game Getter is an interactive application that allows users
-                    to search for over 16,700 video games from various genres,
-                    publishers, release years and more! After searching, a
-                    display of all related search results will be curated just
-                    for you. When clicking on a search result, users may receive
-                    information pertaining to the game such as: available
-                    platforms to play on, the year of release, a brief
-                    description of the game, the publisher, global sales, critic
-                    scores, the developer, the ESRB rating, an image associated
-                    with the game, and a website for more information on about
-                    the game if it exists. If users would like to save any
-                    search result to their own personalized dashboard for
-                    referencing later, they will be prompted to sign up or log
-                    in.
-                </p>
-                <div className=" flex justify-center">
-                    <i  > <SiHandlebarsdotjs size={30}/></i>
-                    <i className="ml-2" > <SiJavascript size={30} color="yellow"/></i>
-                    <i className="ml-2"> <IoLogoCss3  size={30} color="blue"/></i>
-                    <i className="ml-2">  <FaNodeJs  size={30} color="green"/></i>
-                    <i className="ml-2">  < FaHtml5 size={30} color="red"/></i>
-                    <i className="ml-2"> <SiMysql  size={30} color="orange" /></i>
-                </div>
+                    <h3 className=" text-end m-4">Game Getter</h3>
+                    <p className="indent-2 mb-4 mx-4">
+                        Game Getter is an interactive application that allows
+                        users to search for over 16,700 video games from various
+                        genres, publishers, release years and more! After
+                        searching, a display of all related search results will
+                        be curated just for you. When clicking on a search
+                        result, users may receive information pertaining to the
+                        game such as: available platforms to play on, the year
+                        of release, a brief description of the game, the
+                        publisher, global sales, critic scores, the developer,
+                        the ESRB rating, an image associated with the game, and
+                        a website for more information on about the game if it
+                        exists. If users would like to save any search result to
+                        their own personalized dashboard for referencing later,
+                        they will be prompted to sign up or log in.
+                    </p>
+                    <div className=" flex justify-center">
+                        <i>
+                            {" "}
+                            <SiHandlebarsdotjs size={30} />
+                        </i>
+                        <i className="ml-2">
+                            {" "}
+                            <SiJavascript size={30} color="yellow" />
+                        </i>
+                        <i className="ml-2">
+                            {" "}
+                            <IoLogoCss3 size={30} color="blue" />
+                        </i>
+                        <i className="ml-2">
+                            {" "}
+                            <FaNodeJs size={30} color="green" />
+                        </i>
+                        <i className="ml-2">
+                            {" "}
+                            <FaHtml5 size={30} color="red" />
+                        </i>
+                        <i className="ml-2">
+                            {" "}
+                            <SiMysql size={30} color="orange" />
+                        </i>
+                    </div>
                 </div>
             </div>
-            <div className="WhaleWatchersContainer flex flex-row">
-                <h3 className="text-end basis-1/2">Whale Watchers</h3>
+            <div className="WhaleWatchersContainer  flex flex-1 flex-row mb-8">
                 <img
-                    className="WhaleWatchersImage basis-1/2 "
+                    className="WhaleWatchersImage basis-1/2 w-32"
                     src="./images/whale-Watchers.png"
                 />
-            </div>
-            <div className="flex justify-end">
-                <p className="w-1/2 indent-2">
-                    Stock app that allows users who want to day trade , long
-                    term share hold, or even large corperations looking for
-                    partnership the ability to use a stocks ticker (Ex. AAPL for
-                    apple or TSLA for tesla ) and recieve relative news
-                    information on that search stock . It also displays the
-                    stocks data for : Current Price,Change,Percent Change,Daily
-                    Low,Daily High,Open Price and Closed Price using third party
-                    APIs.
-                </p>
+                <div className="basis-1/2">
+                    <h3 className="text-end m-4">Whale Watchers</h3>
+                    <p className="indent-2 mb-4 mx-4">
+                        Stock app that allows users who want to day trade , long
+                        term share hold, or even large corperations looking for
+                        partnership the ability to use a stocks ticker (Ex. AAPL
+                        for apple or TSLA for tesla ) and recieve relative news
+                        information on that search stock . It also displays the
+                        stocks data for : Current Price,Change,Percent
+                        Change,Daily Low,Daily High,Open Price and Closed Price
+                        using third party APIs.
+                    </p>
+                </div>
             </div>
 
             <div className="WorkDaySchedulerContainer flex flex-row">
